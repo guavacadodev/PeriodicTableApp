6bbedfc195aaf4bf343e73ffc8306d0fae614661
Jake Woodall - Guavacado <jakewoodall001@gmail.com>
2025-10-10 02:10:35 -0700

    - update ElementModel to add CodingKeys enum - update ElementCategory cases - enum phase now use capitals

--- PeriodicTableApp/Model/ElementModel.swift
+++ PeriodicTableApp/Model/ElementModel.swift
@@ -18,34 +18,81 @@ struct ElementModel: Identifiable, Codable, Hashable {
     let yearDiscovered: Int?
     let discoveredBy: String?
     let colorHex: String?
+    let bohrModelImage: String?
     /*
      add more properties later such as
      - boiling point
      - electonegativity ...
      */
     
+    enum CodingKeys: String, CodingKey {
+        case name
+        case symbol
+        case atomicNumber
+        case atomicMass
+        case elementCategory
+        case phase
+        case yearDiscovered
+        case discoveredBy
+        case colorHex
+        case bohrModelImage
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        symbol = try container.decode(String.self, forKey: .symbol)
+        atomicNumber = try container.decode(Int.self, forKey: .atomicNumber)
+        atomicMass = try container.decode(Double.self, forKey: .atomicMass)
+        elementCategory = try container.decode(ElementCategory.self, forKey: .elementCategory)
+        phase = try container.decode(Phase.self, forKey: .phase)
+        yearDiscovered = try? container.decode(Int.self, forKey: .yearDiscovered)
+        discoveredBy = try? container.decode(String.self, forKey: .discoveredBy)
+        colorHex = try? container.decode(String.self, forKey: .colorHex)
+        bohrModelImage = try? container.decode(String.self, forKey: .bohrModelImage)
+    }
+    
+    init(name: String, symbol: String, atomicNumber: Int, atomicMass: Double, 
+         elementCategory: ElementCategory, phase: Phase, yearDiscovered: Int?, 
+    discoveredBy: String?, colorHex: String?, bohrModelImage: String?) {
+        self.name = name
+        self.symbol = symbol
+        self.atomicNumber = atomicNumber
+        self.atomicMass = atomicMass
+        self.elementCategory = elementCategory
+        self.phase = phase
+        self.yearDiscovered = yearDiscovered
+        self.discoveredBy = discoveredBy
+        self.colorHex = colorHex
+        self.bohrModelImage = bohrModelImage
+    }
+    
 }
 
 enum ElementCategory: String, Codable {
+    case diatomicNonmetal = "diatomic nonmetal"
+    case polyatomicNonmetal = "polyatomic nonmetal"
+    case alkaliMetal = "alkali metal"
+    case alkalineEarthMetal = "alkaline earth metal"
+    case transitionMetal = "transition metal"
+    case postTransitionMetal = "post-transition metal"
+    case nobleGas = "noble gas"
     case metalloid = "metalloid"
-    case nonmetal = "nonmetal"
-    case alkaliMetal = "alkaliMetal"
-    case alkalineEarthMetal = "alkalineEarthMetal"
-    case transitionMetal = "transitionMetal"
-    case postTransitionMetal = "postTransitionMetal"
-    case halogen = "halogen"
-    case nobleGas = "nobleGas"
     case lanthanide = "lanthanide"
     case actinide = "actinide"
     case unknown = "unknown"
+    case unknownProbablyTransitionMetal = "unknown, probably transition metal"
+    case unknownProbablyPostTransitionMetal = "unknown, probably post-transition metal"
+    case unknownProbablyMetalloid = "unknown, probably metalloid"
+    case unknownPredictedNobleGas = "unknown, predicted to be noble gas"
+    case unknownPredictedAlkaliMetal = "unknown, but predicted to be an alkali metal"
 }
 
 enum Phase: String, Codable {
-    case solid = "solid"
-    case liquid = "liquid"
-    case gas = "gas"
-    case plasma = "plasma"
-    case unknown = "unknown"
+    case solid = "Solid"
+    case liquid = "Liquid"
+    case gas = "Gas"
+    case unknown = "Unknown"
 }
 
 let hydrogen = ElementModel(
@@ -53,11 +100,12 @@ let hydrogen = ElementModel(
     symbol: "H",
     atomicNumber: 1,
     atomicMass: 1.008,
-    elementCategory: .nonmetal,
+    elementCategory: .diatomicNonmetal,
     phase: .gas,
     yearDiscovered: 1766,
     discoveredBy: "Antoine Lavoisier",
     colorHex: "#FFFFFF",
+    bohrModelImage: "https://storage.googleapis.com/search-ar-edu/periodic-table/element_001_hydrogen/element_001_hydrogen_srp_th.png",
 )
 
 let helium = ElementModel(
@@ -65,11 +113,12 @@ let helium = ElementModel(
     symbol: "He",
     atomicNumber: 2,
     atomicMass: 4.0026,
-    elementCategory: .nonmetal,
+    elementCategory: .nobleGas,
     phase: .gas,
     yearDiscovered: 1766,
     discoveredBy: "Antoine Lavoisier",
     colorHex: "#FFFFFF",
+    bohrModelImage: "https://storage.googleapis.com/search-ar-edu/periodic-table/element_001_hydrogen/element_001_hydrogen_srp_th.png",
 )
 
 let lithium = ElementModel(
@@ -82,6 +131,7 @@ let lithium = ElementModel(
     yearDiscovered: 1817,
     discoveredBy: "Martin von Trapp",
     colorHex: "#BFBFBF",
+    bohrModelImage: "https://storage.googleapis.com/search-ar-edu/periodic-table/element_001_hydrogen/element_001_hydrogen_srp_th.png",
 )
 
 let beryllium = ElementModel(
@@ -89,9 +139,10 @@ let beryllium = ElementModel(
     symbol: "Be",
     atomicNumber: 4,
     atomicMass: 9.0122,
-    elementCategory: .alkaliMetal,
+    elementCategory: .alkalineEarthMetal,
     phase: .solid,
     yearDiscovered: 1798,
     discoveredBy: "Daniel Rutherford",
     colorHex: "#AFAFAF",
+    bohrModelImage: "https://storage.googleapis.com/search-ar-edu/periodic-table/element_001_hydrogen/element_001_hydrogen_srp_th.png",
 )
